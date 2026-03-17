92d5a8b1600564ffc81ee8e4ff7d1b37f42eff55
DeemaAhmad100 <Yahoahmad133@gmail.com>
2026-03-18 00:45:37 +0300

    fix import issue and pass tests

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -10,7 +10,7 @@ The autograder will run your tests as part of the CI check.
 """
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath('.'))  # Ensure pipeline.py is importable
 
 import pandas as pd
 import numpy as np
